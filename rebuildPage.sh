e3a08cef9424b4a6b27c9bd129fd1039b4d439ed
leonana69 <leonana69@github.com>
2021-08-31 11:26:59 -0400

    updata email

--- rebuildPage.sh
+++ rebuildPage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -u leonana69: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 -X POST https://api.github.com/repos/leonana69/guojun.chen/pages/builds
+curl -u leonana69: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 -X POST https://api.github.com/repos/leonana69/guojun.chen/pages/builds
