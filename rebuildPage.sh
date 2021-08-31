77a8a4ee884a384a92a0362740ad130f17391402
leonana69 <leonana69@github.com>
2021-08-31 11:18:02 -0400

    delete template

--- rebuildPage.sh
+++ rebuildPage.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -u leonana69: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 -X POST https://api.github.com/repos/leonana69/guojun.chen/pages/builds
+curl -u leonana69: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 -X POST https://api.github.com/repos/leonana69/guojun.chen/pages/builds
